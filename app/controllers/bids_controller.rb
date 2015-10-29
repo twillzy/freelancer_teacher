219f5fdb23baa2a2229e2b76b15e8198b7dd7c01
JadeInHand <cpurcell22@gmail.com>
2015-10-30 09:29:52 +1100

    small styling on project new, added create in bid control

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -5,4 +5,18 @@ class BidsController < ActionController::Base
 		@project = Project.where :id => @bider.first.project_id
 	end
 
+	def create
+		@bid = Bid.new bid_params
+		if @bid.save
+			redirect_to projects_path
+		else
+			render :new
+		end
+	end
+
+
+	private
+	def bid_params
+		params.require(:bid).permit(:bidder_id, :bid_amount, :name, :location, :avatar, :reputation, :skills, :profile, :proposal, :project_id)
+	end
 end
