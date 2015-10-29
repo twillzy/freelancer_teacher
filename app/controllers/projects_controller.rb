1a0120c03c61ff68001d2b3113ecf41017c85100
twillzy <william.tanzy@gmail.com>
2015-10-29 17:55:29 +1100

    projects controller update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,12 +12,30 @@ class ProjectsController < ApplicationController
 	end
 
 	def create
+		@project = Project.new project_params
+		if @project.save
+			redirect_to root_path
+		else
+			render :new
+		end
 	end
 
 	def update
+		@project = Project.find params[:id]
+		@project.update project_params
+		if @project.save
+			redirect_to root_path
+		else
+			render :edit
+		end
 	end
 
 	def edit
+		@project = Project.find params[:id]
 	end
 
+	private
+	def project_params
+		params.require(:project).permit(:title, :description, :currency, :budget, :jobs)
+	end
 end
