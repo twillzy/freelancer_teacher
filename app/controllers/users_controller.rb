b2e457cfc600e207ef6f71ea640d8a0cda577a0b
JadeInHand <cpurcell22@gmail.com>
2015-10-29 17:20:56 +1100

    fixed login issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to projects_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
  
     respond_to do |format|
       if @user.update(user_details)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to projects_path, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
